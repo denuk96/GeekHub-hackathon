4a52cdc3d4348e9c12fcc25e1ca9915a3df6f46c
Denys <denys.taradada.triare@gmail.com>
2020-04-11 18:11:51 +0300

    fix

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,4 +1,12 @@
 RailsAdmin.config do |config|
+  RailsAdmin.config do |config|
+    config.authenticate_with do
+      authenticate_or_request_with_http_basic('Site Message') do |username, password|
+        username == 'admin' && password == 'password'
+      end
+    end
+  end
+
   ### Popular gems integration
 
   ## == Devise ==
