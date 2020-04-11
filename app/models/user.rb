3510e74e38e1a16f8e8519a73ff08ca448a67656
Denys <denys.taradada.triare@gmail.com>
2020-04-11 11:24:48 +0300

    facebook auth added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@
 #  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  image                  :text
+#  name                   :string
+#  provider               :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  uid                    :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
@@ -20,5 +24,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable,
+                                                                                              :omniauth_providers => [:facebook]
+
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if (data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"])
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+    end
+  end
 end
